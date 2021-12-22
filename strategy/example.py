6c506ade7be379bf98d652cb4e1c9adeadd0bbe4
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-12-22 13:09:57 +0800

    feature(Strategy): 增加策略交易示例

--- strategy/example.py
+++ strategy/example.py
@@ -4,8 +4,11 @@
 @file example.py
 @desc:
 策略示例，主要展示api调用
+模拟一个简单策略：判断当前涨跌幅，如果当前上涨幅度小于2%，则开一个多单，然后半小时后平掉
 """
 import time
+
+import arrow
 
 from broker import RedisBrokerConfig
 from disptach import MemoryDispatcher
@@ -14,7 +17,7 @@ from ftdc.structs import UserLoginField
 
 
 class StrategyExample(object):
-    max_limit = 0
+    close_time = None
 
     def __init__(self, user: UserLoginField, redis_config: RedisBrokerConfig, contract_id: str):
         self.contract = contract_id
@@ -29,12 +32,17 @@ class StrategyExample(object):
 
     def trade(self):
         for quote in self.query_quote():
-            # 仅允许下2单
-            if self.max_limit >= 2:
-                break
-            # 下单
+            if self.close_time:
+                now = arrow.now()
+                if now >= self.close_time:
+                    lower = float(quote['lower'])
+                    order = self.tm.buy_close(self.contract, 'SHFE', lower, 1)
+                    return order
+                continue
             upper = float(quote['upper'])  # 涨停价
-            order = self.tm.buy_open(self.contract, 'SHFE', upper, 1)
-            self.max_limit += 1
-            yield order
-            time.sleep(60*30)
+            chg = float(quote['close']) / float(quote['open']) - 1
+            if chg <= 0.02:
+                order = self.tm.buy_open(self.contract, 'SHFE', upper, 1)
+                now = arrow.now()
+                self.close_time = now.shift(minutes=30)
+                yield order
